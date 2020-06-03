7a115c60d5254ca35d7755d1f55d8f0f831ff7d5
atroliveira92 <atroliveira92@gmail.com>
2020-06-02 23:00:42 -0300

    Fix size of selected date circle area

--- marscalendar/src/main/java/com/arthur/marscalendar/view/daypicker/DayPickerAdapter.kt
+++ marscalendar/src/main/java/com/arthur/marscalendar/view/daypicker/DayPickerAdapter.kt
@@ -10,12 +10,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.arthur.marscalendar.R
 import kotlinx.android.synthetic.main.day_row_view.view.*
 
-class DayPickerAdapter(var days: List<Day>, private val height: Int, private val listener: OnDayPickerListener): RecyclerView.Adapter<DayPickerAdapter.DayPickerViewHolder>() {
+class DayPickerAdapter(var days: List<Day>, private val size: Int, private val listener: OnDayPickerListener): RecyclerView.Adapter<DayPickerAdapter.DayPickerViewHolder>() {
     private var selectedPos: Int = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayPickerViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.day_row_view, parent, false)
-        view.minimumHeight = height
+        view.minimumHeight = size
         return DayPickerViewHolder(view)
     }
 
@@ -46,6 +46,10 @@ class DayPickerAdapter(var days: List<Day>, private val height: Int, private val
             itemView.mTextViewDay.text = if(day.value == 0) "" else day.value.toString()
             itemView.mTextViewDay.setTextColor(if (day.disabled) ContextCompat.getColor(itemView.context, R.color.disabled_date)
                                                 else ContextCompat.getColor(itemView.context, R.color.enbaled_date))
+
+            itemView.mFrameSelected.layoutParams.height = size.div(1.5).toInt()
+            itemView.mFrameSelected.layoutParams.width = size.div(1.5).toInt()
+
             itemView.isEnabled = !day.disabled
             if (day.selected) {
                 itemView.mFrameSelected.background = ContextCompat.getDrawable(itemView.context, R.drawable.selected_date)
